1d0d5e7fc9754bab46828626a05cf9d08966c47f
Adem Budak <adembudak@outlook.com>
2026-03-09 23:28:27 +0300

    A window!

--- main.cpp
+++ main.cpp
@@ -6,4 +6,47 @@
 #include <imgui_impl_glfw.h>
 #include <stb_image.h>
 
-int main() {}
+int main(int argc, const char* argv[]) {
+  /* Initialize the library */
+  if(!glfwInit())
+    return -1;
+
+  glfwWindowHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if(!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+  glewInit();
+
+  /*
+    glfwSetWindowSizeCallback(window, glfw_onResize);
+    glfwSetKeyCallback(window, glfw_onKey);
+    glfwSetMouseButtonCallback(window, glfw_onMouseButton);
+    glfwSetCursorPosCallback(window, glfw_onMouseMove);
+    glfwSetScrollCallback(window, glfw_onMouseWheel);
+  */
+
+  /* Loop until the user closes the window */
+  while(!glfwWindowShouldClose(window)) {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
